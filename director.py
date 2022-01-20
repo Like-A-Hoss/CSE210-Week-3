d8c033251dcf259588141ab0768d7e3f0d113540
Like-A-Hoss <hapleswithdice@gmail.com>
2022-01-19 21:03:19 -0600

    Updated Director.  Now prints things.

--- director.py
+++ director.py
@@ -1,1 +1,18 @@
+import hider, seeker, terminal
 
+def main():
+    #starting this train wreck up
+    baddie = hider.hider()
+    player = seeker.seeker()
+    inandout = terminal.terminal()
+    playing = True
+    #Great let's start this game.
+    inandout.print_response("Welcome to Hide and Seek")
+    inandout.print_response("Catch the killer before he can run away")
+    while playing == True:
+        
+    
+
+
+if name =="__main__":
+    main()
